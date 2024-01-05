3bccd2aa6ffe2a376cb1df24326f98eee7ef435a
Suphon Thanakornpakapong <8080853+suphon-t@users.noreply.github.com>
2024-01-05 23:49:28 +0700

    feat: swift grpc

--- DotLocal/DaemonManager.swift
+++ DotLocal/DaemonManager.swift
@@ -6,6 +6,8 @@
 //
 
 import Foundation
+import GRPC
+import NIO
 
 class DaemonManager: ObservableObject {
     static let shared = DaemonManager()
@@ -14,6 +16,8 @@ class DaemonManager: ObservableObject {
     private let daemonUrl = Bundle.main.bundleURL.appendingPathComponent("Contents/Resources/dotlocal-daemon")
     @Published var state: DaemonState = .stopped
     private var task: Process? = nil
+    private(set) var apiClient: DotLocalAsyncClient? = nil
+    private var group: EventLoopGroup? = nil
     
     private init() {
     }
@@ -43,7 +47,7 @@ class DaemonManager: ObservableObject {
             print(chunk, terminator: "")
             if chunk.contains("API server listening") {
                 DispatchQueue.main.async {
-                    self.state = .started
+                    self.onStart()
                 }
             }
             handle.waitForDataInBackgroundAndNotify()
@@ -59,8 +63,24 @@ class DaemonManager: ObservableObject {
         self.task = task
     }
     
+    private func onStart() {
+        let socketPath = FileManager.default.homeDirectoryForCurrentUser.appendingPathComponent(".dotlocal/api.sock")
+        let group = PlatformSupport.makeEventLoopGroup(loopCount: 1)
+        self.group = group
+        // TODO: try catch
+        let channel = try! GRPCChannelPool.with(
+            target: .unixDomainSocket(socketPath.path(percentEncoded: false)),
+            transportSecurity: .plaintext,
+            eventLoopGroup: group
+        )
+        let apiClient = DotLocalAsyncClient(channel: channel)
+        self.apiClient = apiClient
+        state = .started
+    }
+    
     private func onStop() {
         task = nil
+        apiClient = nil
         state = .stopped
     }
     
