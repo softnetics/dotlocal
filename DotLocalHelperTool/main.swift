f98d120562c5605785ad1030c0e223f4bf3bbf72
Suphon Thanakornpakapong <8080853+suphon-t@users.noreply.github.com>
2024-01-12 20:37:40 +0700

    feat: terminate daemon before exiting helper

--- DotLocalHelperTool/main.swift
+++ DotLocalHelperTool/main.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import SecureXPC
+import Dispatch
 
 func parentAppURL() throws -> URL {
     let components = Bundle.main.bundleURL.pathComponents
@@ -34,10 +35,7 @@ if getppid() == 1 {
     server.registerRoute(SharedConstants.installClientRoute, handler: ManageClient.install)
     server.registerRoute(SharedConstants.uninstallClientRoute, handler: ManageClient.uninstall)
     
-    server.registerRoute(SharedConstants.exitRoute, handler: {
-        NSLog("exiting")
-        exit(0)
-    })
+    server.registerRoute(SharedConstants.exitRoute, handler: gracefulExit)
     server.setErrorHandler { error in
         if case .connectionInvalid = error {
             // Ignore invalidated connections as this happens whenever the client disconnects which is not a problem
@@ -45,7 +43,24 @@ if getppid() == 1 {
             NSLog("error: \(error)")
         }
     }
+    
+    signal(SIGINT, SIG_IGN)
+    signal(SIGTERM, SIG_IGN)
+    
+    let sigintSrc = DispatchSource.makeSignalSource(signal: SIGINT, queue: .main)
+    sigintSrc.setEventHandler(handler: gracefulExit)
+    sigintSrc.resume()
+    let sigtermSrc = DispatchSource.makeSignalSource(signal: SIGTERM, queue: .main)
+    sigtermSrc.setEventHandler(handler: gracefulExit)
+    sigtermSrc.resume()
+    
     server.startAndBlock()
 } else {
     print("not supported")
+}
+
+func gracefulExit() {
+    NSLog("exiting")
+    DaemonManager.shared.stop()
+    exit(0)
 }
